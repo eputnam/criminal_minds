96870b601642710686d628ed23148e58f9faf9fa
Putnam <putnam.eric@gmail.com>
2016-07-07 20:59:42 -0700

    adds stuff, changes repo name

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,18 @@
 class criminal_minds {
 
-	$testdir		= "/etc/testdir"
+	$configdir		= "/etc/criminal_minds"
+	$configfile		= "team_members=7\nstarting_location=FBI_HQ\ndestination='Philadelphia, PA'"
 
 	notify { "Wheels up in 30!": }
-	file { $testdir :
+
+	file { '/etc/testdir':
+		ensure => absent
+	}
+	file { $configdir :
 		ensure => directory,
 	}
-	file { "${testdir}/helloworld":
+	file { "${configdir}/criminal_minds.conf":
 		ensure => file
+		content => $configfile
 	}
 }
