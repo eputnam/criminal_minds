5faa59e698cd840ee3fe1eabf0c45e73d2bbdab3
Putnam <putnam.eric@gmail.com>
2016-07-07 22:00:23 -0700

    attempts long-shot at loading team.yaml

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,10 @@
 class criminal_minds {
 
+	include 'stdlib'
+
 	$configdir		= "/etc/criminal_minds"
 	$configfile		= "team_members=7\nstarting_location=FBI_HQ\ndestination='Philadelphia, PA'"
+	$team_data		= loadyaml('criminal_minds/lib/team.yaml')
 
 	notify { "Wheels up in 30!": }
 
@@ -16,8 +19,10 @@ class criminal_minds {
 		ensure 		=> file,
 		content 	=> $configfile,
 	}
-	criminal_minds::team_member { 'sreid':
-		member_name 		=> "Spencer Reid",
-		characteristics 	=> ["smart", "dorky", "well-kept"]
+	$team_data.each | String $member | {
+		criminal_minds::team_member { "$member":
+		member_name			=> "${member.member_name}"
+		characteristics		=> "${member.characteristics}"
+		}
 	}
 }
--- manifests/team_member.pp
+++ manifests/team_member.pp
@@ -6,7 +6,7 @@ define criminal_minds::team_member(
             fail('base class must be included!')
         }
 
-        file { '/var/www/${member_name}.html':
+        file { "/var/www/${member_name}.html":
             ensure => file,
             content => epp('criminal_minds/team_member.epp', {'member_name' => $member_name, 'characteristics' => $characteristics}),
         }
