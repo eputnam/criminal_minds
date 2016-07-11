f2990e455e6d84dacda7d6686fee3516f5f82a2b
Putnam <putnam.eric@gmail.com>
2016-07-10 18:17:43 -0700

    gets rid of the yaml for now

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,25 @@ class criminal_minds {
 	include 'stdlib'
 
 	$configdir		= "/etc/criminal_minds"
-	$configfile		= "team_members=7\nstarting_location=FBI_HQ\ndestination='Philadelphia, PA'"
-	$team_data		= parseyaml('/etc/puppetlabs/code/environments/production/modules/criminal_minds/lib/team.yaml')
+	$configfile		= "team_members=7\nstarting_location=FBI_HQ\ndestination='Philadelphia, PA'\n"
+	$docroot		= "/var/www"
+	$team_data		= {
+		{
+			"member_id" => "sreid",
+			"member_name" => "Spencer Reid",
+			"characteristics" => ["smart", "awkward", "well-kept"]
+		},
+		{
+			"member_id" => "ahotchner",
+			"member_name" => "Aaron Hotchner",
+			"characteristics" => ["firm", "leader", "compassionate"]
+		},
+		{
+			"member_id" => "dmorgan",
+			"member_name" => "Derek Morgan",
+			"characteristics" => ["hot-headed", "old-fashioned", "alpha"]
+		}
+	}
 
 	notify { "Wheels up in 30!": }
 
@@ -19,11 +36,15 @@ class criminal_minds {
 		content 	=> $configfile,
 	}
 
-	notify { "$team_data": }
-	# $team_data.each | $member | {
-	# 	criminal_minds::team_member { %{$member.member_id} :
-	# 	member_name			=> %{$member.member_name},
-	# 	characteristics		=> %{$member.characteristics},
-	# 	}
-	# }
+	file { "${docroot}/index.html":
+		ensure 		=> file,
+		content		=> epp("criminal_minds/index.epp",{ "team_members" => $team_data })
+	}
+
+	$team_data.each | $member | {
+		criminal_minds::team_member { $member.member_id :
+		member_name			=> $member.member_name,
+		characteristics		=> $member.characteristics,
+		}
+	}
 }
