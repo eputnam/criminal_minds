5ace9e9a2d8e6b1378de57b3e106e45b2d626507
eputnam <putnam.eric@gmail.com>
2016-07-11 21:11:22 -0700

    applies some polish

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,10 @@
 class criminal_minds {
         include 'stdlib'
-
+	
+	$docroot 		= "/var/www"
         $configdir              = "/etc/criminal_minds"
         $configfile             = "team_members=7\nstarting_location=FBI_HQ\ndestination='Philadelphia, PA'\n"
-        # $team_data              = {
-        #         "member" =>
-        #                 { "member_id" => "sreid", "member_name" => "Spencer Reid", "characteristics" => ["smart", "awkward", "well-kept"]},
-        #         "member2" =>
-        #                 { "member_id" => "ahotchner", "member_name" => "Aaron Hotchner", "characteristics" => ["firm", "leader", "compassionate"]},
-        #         "member3" =>
-        #                 { "member_id" => "dmorgan", "member_name" => "Derek Morgan", "characteristics" => ["hot-headed", "old-fashioned", "alpha"]}
-        # }
-		$team_data = loadyaml('/etc/puppetlabs/code/environments/production/modules/criminal_minds/lib/team.yaml')
+	$team_data 		= loadyaml("/etc/puppetlabs/code/environments/production/modules/criminal_minds/lib/team.yml")
 
         notify { "Wheels up in 30!": }
 
@@ -33,7 +26,7 @@ class criminal_minds {
         }
 
         $team_data.each | $member, $info | {
-                criminal_minds::team_member { $info[member_id] :
+                criminal_minds::team_member { $member :
                 member_name             => $info[member_name],
                 characteristics         => $info[characteristics],
                 }
