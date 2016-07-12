c1d5d2cd5b391f9290b579716b0380b1b872559d
Putnam <putnam.eric@gmail.com>
2016-07-11 20:37:30 -0700

    syncs up with server changes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,41 +1,41 @@
 class criminal_minds {
-	include 'stdlib'
+        include 'stdlib'
 
-	$configdir		= "/etc/criminal_minds"
-	$configfile		= "team_members=7\nstarting_location=FBI_HQ\ndestination='Philadelphia, PA'\n"
-	$docroot		= "/var/www"
-	$team_data		= {
-		"member" =>
-			{ "member_id" => "sreid", "member_name" => "Spencer Reid", "characteristics" => ["smart", "awkward", "well-kept"]},
-		"member2" =>
-			{ "member_id" => "ahotchner", "member_name" => "Aaron Hotchner", "characteristics" => ["firm", "leader", "compassionate"]},
-		"member3" =>
-			{ "member_id" => "dmorgan", "member_name" => "Derek Morgan", "characteristics" => ["hot-headed", "old-fashioned", "alpha"]}
-	}
+        $configdir              = "/etc/criminal_minds"
+        $configfile             = "team_members=7\nstarting_location=FBI_HQ\ndestination='Philadelphia, PA'\n"
+        $docroot                = "/var/www"
+        $team_data              = {
+                "member" =>
+                        { "member_id" => "sreid", "member_name" => "Spencer Reid", "characteristics" => ["smart", "awkward", "well-kept"]},
+                "member2" =>
+                        { "member_id" => "ahotchner", "member_name" => "Aaron Hotchner", "characteristics" => ["firm", "leader", "compassionate"]},
+                "member3" =>
+                        { "member_id" => "dmorgan", "member_name" => "Derek Morgan", "characteristics" => ["hot-headed", "old-fashioned", "alpha"]}
+        }
 
-	notify { "Wheels up in 30!": }
+        notify { "Wheels up in 30!": }
 
-	file { '/etc/testdir':
-		ensure 		=> absent,
-		force 		=> true,
-	}
-	file { $configdir :
-		ensure 		=> directory,
-	}
-	file { "${configdir}/criminal_minds.conf":
-		ensure 		=> file,
-		content 	=> $configfile,
-	}
+        file { '/etc/testdir':
+                ensure          => absent,
+                force           => true,
+        }
+        file { $configdir :
+                ensure          => directory,
+        }
+        file { "${configdir}/criminal_minds.conf":
+                ensure          => file,
+                content         => $configfile,
+        }
 
-	file { "${docroot}/index.html":
-		ensure 		=> file,
-		content		=> epp("criminal_minds/index.epp",{ "team_members" => $team_data })
-	}
+        file { "${docroot}/index.html":
+                ensure          => file,
+                content         => epp("criminal_minds/index.epp",{ "team_members" => $team_data })
+        }
 
-	$team_data.each | $member | {
-		criminal_minds::team_member { $member.member_id :
-		member_name			=> $member.member_name,
-		characteristics		=> $member.characteristics,
-		}
-	}
+        $team_data.each | $otherthing,$member | {
+                criminal_minds::team_member { $member[member_id] :
+                member_name             => $member[member_name],
+                characteristics         => $member[characteristics],
+                }
+        }
 }
--- manifests/team_member.pp
+++ manifests/team_member.pp
@@ -1,4 +1,5 @@
 define criminal_minds::team_member(
+    $member_id          = $name,
     $member_name        = "mr. default",
     $characteristics    = ["tall", "smart", "funny"],
     ) {
@@ -6,7 +7,7 @@ define criminal_minds::team_member(
             fail('base class must be included!')
         }
 
-        file { "/var/www/${member_name}.html":
+        file { "/var/www/${member_id}.html":
             ensure => file,
             content => epp('criminal_minds/team_member.epp', {'member_name' => $member_name, 'characteristics' => $characteristics}),
         }
